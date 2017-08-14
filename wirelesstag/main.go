bde7ea6d254bf0c53893b8733ec8706a7197cc4b
John Maguire <john.maguire@gmail.com>
2017-08-14 12:16:50 +0100

    Cache oauth tokens

--- wirelesstag/main.go
+++ wirelesstag/main.go
@@ -10,20 +10,25 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"os/user"
+	"path/filepath"
 	"runtime"
+	"time"
 
 	"github.com/dchest/uniuri"
 	"golang.org/x/oauth2"
 )
 
-var clientSecret = flag.String("secret", "OAuth2 client secret for WirelessTag", "")
-var clientId = flag.String("client", "OAuth2 client id for WirelessTag", "")
+var clientSecret = flag.String("secret", "", "OAuth2 client secret for WirelessTag")
+var clientId = flag.String("client", "", "OAuth2 client id for WirelessTag")
 
-func exchangeToken(code string) (*oauth2.Token, error) {
-	response, err := http.PostForm("https://www.mytaglist.com/oauth2/access_token.aspx",
+func exchangeToken(config *oauth2.Config, code string) (*oauth2.Token, error) {
+	response, err := http.PostForm(
+		config.Endpoint.TokenURL,
 		url.Values{
-			"client_id":     {*clientId},
-			"client_secret": {*clientSecret},
+			"client_id":     {config.ClientID},
+			"client_secret": {config.ClientSecret},
 			"code":          {code},
 		})
 	if err != nil {
@@ -38,7 +43,89 @@ func exchangeToken(code string) (*oauth2.Token, error) {
 	return &token, nil
 }
 
+func tokenCacheFile() (string, error) {
+	usr, err := user.Current()
+	if err != nil {
+		return "", fmt.Errorf("Failed to get cache file: %v", err)
+	}
+	tokenCacheDir := filepath.Join(usr.HomeDir, ".credentials")
+	os.MkdirAll(tokenCacheDir, 0700)
+	return filepath.Join(tokenCacheDir, url.QueryEscape("mytaglist.json")), nil
+}
+
+func saveToken(file string, token *oauth2.Token) {
+	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to open token cache file: %v", err)
+	}
+	defer f.Close()
+	t := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(t)
+	return t, err
+}
+
+func tokenFromWeb(ctx context.Context, config *oauth2.Config) (token *oauth2.Token, err error) {
+	state := uniuri.New()
+	mux := http.NewServeMux()
+	srv := &http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		log.Printf("Request: %v\n", r.URL)
+		retState := r.URL.Query().Get("state")
+		if retState != state {
+			http.Error(w, "State inconsistent", 400)
+			log.Fatalf("State does not match. Expected: %s Got: %s\n", state, retState)
+		}
+		code := r.URL.Query().Get("code")
+		token, err = exchangeToken(config, code)
+		if err != nil {
+			http.Error(w, "Oops!", 400)
+			log.Fatalf("Failed to exchange token: %v", err)
+		}
+		stopCtx, cancel := context.WithDeadline(ctx, time.Now().Add(2*time.Second))
+		defer cancel()
+		srv.Shutdown(stopCtx)
+	})
+
+	url := config.AuthCodeURL(state, oauth2.AccessTypeOffline)
+	fmt.Printf("Visit the URL for the auth dialog: %v", url)
+	srv.ListenAndServe()
+	return token, nil
+}
+
+func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
+	cacheFile, err := tokenCacheFile()
+	if err != nil {
+		log.Fatalf("Unable to get token cache file: %v", err)
+	}
+	token, err := tokenFromFile(cacheFile)
+	if err != nil {
+		token, err = tokenFromWeb(ctx, config)
+		if err != nil {
+			log.Fatalf("Unable to get token from web: %v", err)
+		}
+		saveToken(cacheFile, token)
+	}
+	return config.Client(ctx, token)
+}
+
 func main() {
+	flag.Parse()
 	runtime.GOMAXPROCS(12)
 
 	ctx := context.Background()
@@ -53,41 +140,16 @@ func main() {
 		RedirectURL: "http://localhost:8080",
 	}
 
-	state := uniuri.New()
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			http.NotFound(w, r)
-			return
-		}
-		log.Printf("Request: %v\n", r.URL)
-		retState := r.URL.Query().Get("state")
-		if retState != state {
-			http.Error(w, "State inconsistent", 400)
-			log.Fatalf("State does not match. Expected: %s Got: %s\n", state, retState)
-		}
-		code := r.URL.Query().Get("code")
-		token, err := exchangeToken(code)
-		if err != nil {
-			http.Error(w, "Oops!", 400)
-			log.Fatalf("Failed to exchange token: %v", err)
-		}
-		client := conf.Client(ctx, token)
-		json := []byte(`{id:1, beepDuration:1}`)
-		resp, err := client.Post("https://www.mytaglist.com/ethClient.asmx/Beep", "application/json", bytes.NewBuffer(json))
-		if err != nil {
-			log.Fatalf("Failed to fetch stuff: %v", err)
-		}
-		data, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatalf("Failed to read body: %v", err)
-		}
-		resp.Body.Close()
-		fmt.Printf("%s\n", data)
-	})
-
-	url := conf.AuthCodeURL(state, oauth2.AccessTypeOffline)
-	fmt.Printf("Visit the URL for the auth dialog: %v", url)
-
-	http.ListenAndServe("localhost:8080", nil)
+	client := getClient(ctx, conf)
+	json := []byte(`{id:1, beepDuration:1}`)
+	resp, err := client.Post("https://www.mytaglist.com/ethClient.asmx/Beep", "application/json", bytes.NewBuffer(json))
+	if err != nil {
+		log.Fatalf("Failed to fetch stuff: %v", err)
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed to read body: %v", err)
+	}
+	resp.Body.Close()
+	fmt.Printf("%s\n", data)
 }
