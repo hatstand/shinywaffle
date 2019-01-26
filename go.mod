63d27db44228e667528eeac962d31edf115aaa6c
John Maguire <john.maguire@gmail.com>
2019-01-26 22:25:20 +0000

    Working pixel font inky

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/andybalholm/cascadia v1.0.0 // indirect
 	github.com/blend/go-sdk v0.3.1 // indirect
 	github.com/coreos/go-systemd v0.0.0-20181012123002-c6f51f82210d
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/davecgh/go-spew v1.1.1
 	github.com/dchest/uniuri v0.0.0-20160212164326-8902c56451e9
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/felixge/pidctrl v0.0.0-20160307080219-7b13bcae7243
@@ -20,16 +20,20 @@ require (
 	github.com/jtolds/gls v4.2.1+incompatible // indirect
 	github.com/kidoman/embd v0.0.0-20170508013040-d3d8c0c5c68d
 	github.com/patrickmn/go-cache v2.1.0+incompatible
+	github.com/pbnjay/pixfont v0.0.0-20190124142447-a842e97a7f59 // indirect
 	github.com/sigurn/crc8 v0.0.0-20160107002456-e55481d6f45c
 	github.com/sigurn/utils v0.0.0-20151230205143-f19e41f79f8f // indirect
 	github.com/smartystreets/assertions v0.0.0-20180927180507-b2de0cb4f26d // indirect
 	github.com/smartystreets/goconvey v0.0.0-20180222194500-ef6db91d284a
 	github.com/wcharczuk/go-chart v2.0.1+incompatible
 	go.opencensus.io v0.18.0 // indirect
-	golang.org/x/image v0.0.0-20181025233408-2a2258ff36e9 // indirect
+	golang.org/x/image v0.0.0-20181025233408-2a2258ff36e9
 	golang.org/x/net v0.0.0-20181029044818-c44066c5c816
 	golang.org/x/oauth2 v0.0.0-20181017192945-9dcd33a902f4
 	google.golang.org/api v0.0.0-20181029172801-aff2d7231e3c
 	google.golang.org/genproto v0.0.0-20181029155118-b69ba1387ce2
 	google.golang.org/grpc v1.16.0
+	periph.io/x/periph v3.4.0+incompatible
 )
+
+replace periph.io/x/periph => github.com/hatstand/periph v3.3.1-0.20181219172215-ef3b63494710+incompatible
--- status/cmd/status.go
+++ status/cmd/status.go
@@ -9,32 +9,48 @@ import (
 	"image/png"
 	"log"
 	"os"
+	"os/signal"
+	"sync"
+	"time"
 
 	"github.com/hatstand/shinywaffle/wirelesstag"
+	"github.com/pbnjay/pixfont"
 
-	"golang.org/x/image/font"
-	"golang.org/x/image/font/inconsolata"
-	"golang.org/x/image/math/fixed"
+	"periph.io/x/periph/conn/gpio/gpioreg"
+	"periph.io/x/periph/conn/spi/spireg"
+	"periph.io/x/periph/experimental/devices/inky"
+	"periph.io/x/periph/host"
 )
 
 var path = flag.String("template", "template.png", "Path to template image")
 
-var drawFont = inconsolata.Regular8x16
-
 func drawLabel(m draw.Image, data string, x, y int) {
-	drawer := font.Drawer{
-		Dst:  m,
-		Src:  image.NewUniform(color.RGBA{255, 255, 255, 255}),
-		Face: drawFont,
-		Dot:  fixed.Point26_6{fixed.Int26_6(x * 64), fixed.Int26_6(y * 64)},
-	}
-	drawer.DrawString(data)
+	pixfont.DrawString(m, x, y, data, color.White)
 }
 
 func main() {
 	flag.Parse()
 
-	log.Printf("Font: %+v", drawFont)
+	state, err := host.Init()
+	if err != nil {
+		log.Fatalf("Failed to init periph: %v", err)
+	}
+	log.Printf("%+v", state)
+	log.Printf("%+v", spireg.All())
+
+	port, err := spireg.Open("")
+	if err != nil {
+		log.Fatalf("Failed to open SPI port: %v", err)
+	}
+	dc := gpioreg.ByName("22")
+	reset := gpioreg.ByName("27")
+	busy := gpioreg.ByName("17")
+
+	dev, err := inky.New(port, dc, reset, busy, inky.Red)
+	if err != nil {
+		log.Fatalf("Failed to open inky: %v", err)
+	}
+	dev.SetBorder(inky.Black)
 
 	file, err := os.Open(*path)
 	if err != nil {
@@ -46,29 +62,37 @@ func main() {
 		log.Fatalf("Failed to decode %s as png: %v", *path, err)
 	}
 
-	b := img.Bounds()
-	m := image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
-	draw.Draw(m, m.Bounds(), img, b.Min, draw.Src)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
 
-	out, err := os.Create("out.png")
-	if err != nil {
-		log.Fatalf("Failed to create output file: %v", err)
-	}
+	var mu sync.Mutex
 
-	tags, err := wirelesstag.GetTags("foo", "bar")
-	if err != nil {
-		log.Fatalf("Failed to fetch tags: %v", err)
-	}
-	for i, v := range tags {
-		drawLabel(m, fmt.Sprintf("%s: %.1f°C", v.Name, v.Temperature), 32, drawFont.Height*(i+1))
-	}
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			go func() {
+				mu.Lock()
+				defer mu.Unlock()
+				b := img.Bounds()
+				m := image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
+				draw.Draw(m, m.Bounds(), img, b.Min, draw.Src)
 
-	if err := png.Encode(out, m); err != nil {
-		out.Close()
-		log.Fatalf("Failed to encode output: %v", err)
-	}
+				tags, err := wirelesstag.GetTags("foo", "bar")
+				if err != nil {
+					log.Fatalf("Failed to fetch tags: %v", err)
+				}
+				for i, v := range tags {
+					s := fmt.Sprintf("%s: %.1f°C", v.Name, v.Temperature)
+					log.Println(s)
+					drawLabel(m, s, 32, 8*(i+1))
+				}
 
-	if err := out.Close(); err != nil {
-		log.Fatalf("Failed to close output file: %v", err)
+				dev.Draw(m.Bounds(), m, image.Point{0, 0})
+			}()
+		case <-c:
+			return
+		}
 	}
 }
