20c32b5f184e54e93c20633b1d7ce556067da26d
Lars Lehtonen <lars.lehtonen@gmail.com>
2020-08-01 21:59:58 -0700

    wirelesstag: fix dropped GetLogs() error

--- wirelesstag/wirelesstag.go
+++ wirelesstag/wirelesstag.go
@@ -227,6 +227,9 @@ func GetLogs(clientId string, clientSecret string) (map[string]map[string][]floa
 	}
 
 	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read response: %v", err)
+	}
 	resp.Body.Close()
 
 	var logs StatsResponse
