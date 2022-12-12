16af50a4fa6e08f7c2566e68c852dc0528b329b6
John Maguire <john.maguire@gmail.com>
2022-12-12 20:47:17 +0000

    better logging

--- calendar/schedule.go
+++ calendar/schedule.go
@@ -3,12 +3,12 @@ package calendar
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/patrickmn/go-cache"
+	"go.uber.org/zap"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -19,7 +19,7 @@ import (
 func getClient(config *oauth2.Config) (*http.Client, error) {
 	tok, err := tokenFromEnv()
 	if err != nil {
-		return nil, fmt.Errorf("Failed to get calendar token from env: %v", err)
+		return nil, fmt.Errorf("failed to get calendar token from env: %w", err)
 	}
 	return config.Client(context.Background(), tok), nil
 }
@@ -35,26 +35,28 @@ func tokenFromEnv() (*oauth2.Token, error) {
 type CalendarScheduleService struct {
 	cache   *cache.Cache
 	service *calendar.Service
+	logger  *zap.SugaredLogger
 }
 
-func NewCalendarScheduleService() (*CalendarScheduleService, error) {
+func NewCalendarScheduleService(logger *zap.SugaredLogger) (*CalendarScheduleService, error) {
 	// If modifying these scopes, delete your previously saved token.json.
 	config, err := google.ConfigFromJSON([]byte(os.Getenv("GOOGLE_CREDENTIALS")), calendar.CalendarReadonlyScope)
 	if err != nil {
-		return nil, fmt.Errorf("Unable to parse client secret file to config: %v", err)
+		return nil, fmt.Errorf("unable to parse client secret file to config: %w", err)
 	}
 	client, err := getClient(config)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to get calendar client: %v", err)
+		return nil, fmt.Errorf("failed to get calendar client: %w", err)
 	}
 
 	srv, err := calendar.New(client)
 	if err != nil {
-		return nil, fmt.Errorf("Unable to retrieve Calendar client: %v", err)
+		return nil, fmt.Errorf("unable to retrieve Calendar client: %w", err)
 	}
 	return &CalendarScheduleService{
 		cache:   cache.New(time.Minute*10, time.Minute*20),
 		service: srv,
+		logger:  logger,
 	}, nil
 }
 
@@ -73,10 +75,10 @@ func (srv *CalendarScheduleService) GetSchedule(calendarId string) ([]*calendar.
 		TimeMin: time.Now().Add(time.Hour * -1).Format(time.RFC3339),
 		TimeMax: time.Now().Add(time.Hour * 24 * 7).Format(time.RFC3339),
 	}
-	log.Printf("Fetching calendar for %s", calendarId)
+	srv.logger.Infof("Fetching calendar for %s", calendarId)
 	resp, err := srv.service.Freebusy.Query(request).Do()
 	if err != nil {
-		return nil, fmt.Errorf("Failed to request free/busy: %v", err)
+		return nil, fmt.Errorf("failed to request free/busy: %w", err)
 	}
 	srv.cache.Set(calendarId, resp.Calendars[calendarId].Busy, cache.DefaultExpiration)
 	return resp.Calendars[calendarId].Busy, nil
